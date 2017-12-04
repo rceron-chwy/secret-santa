54934f46623cc75a9408a35e6566d09c6ff53585
Rafael Ceron <rfceron@gmail.com>
2017-12-03 20:26:35 -0500

    mail

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,46 +1,63 @@
 import express from 'express';
 import nodemailer from 'nodemailer';
 import Users from 'data/Users';
+import crypto from 'crypto';
+
+const algorithm = 'aes-256-ctr';
+const salt = 'FN33HNHFcqgaj4x';
 
 const router = express.Router();
 
 // return a list of tags
-router.get('/users', function (req, res, next) {
-    res.json(Users);
-});
-
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'youremail@gmail.com',
-    pass: 'yourpassword'
+    user: 'rfceron@gmail.com',
+    pass: 'ybriwltwvykfgafw'
   }
 });
 
-router.get('/xmas', function (req, res, next) {
+router.get('/reveal/:hash', (req, res, next) => {
+    const encrypted = req.params.hash;
+    res.json(decrypt(encrypted));
+});
+
+
+router.get('/xmas', (req, res, next) => {
 
     const participants = Users.map(user => ({ ...user }));
-    // console.log(participants);
 
-    const secret = participants.map(participant => ({
-            [participant.displayName]: pickSecretFor(participant, participants)
-        })
-    );
+    const secret = participants.map(participant => {
 
-    const mailOptions = {
-        from: 'youremail@gmail.com',
-        to: 'myfriend@yahoo.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-    };
+            const selectedSecret = pickSecretFor(participant, participants);
+            const encrypted = encrypt(selectedSecret.displayName);
+            const subject = 'Secret Santa na casa do Belu e da Nini! 🎅 ❄️ 🎄';
+            const html = `<h2>Oi ${participant.displayName}!!<h2/><p style="font-size: 12px">Clique no link abaixo pra reveler o amigo screto!</p><a target="_blank" href="https://rafaelceron.com/secretsanta/${encrypted}">AMIGO SECRETO</a>`;
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
+            const mailOptions = {
+                from: 'rfceron+secretsanta@gmail.com',
+                to: 'rfceron@gmail.com',
+                subject,
+                html
+            };
+
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+
+            return {
+                [participant.displayName]: {
+                    ...selectedSecret,
+                    encrypted,
+                    decrypted: decrypt(encrypted)
+                }
+            }
         }
-    });
+    );
 
     res.json(secret);
 });
@@ -73,4 +90,18 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,salt)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,salt)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+ 
 module.exports = router;
